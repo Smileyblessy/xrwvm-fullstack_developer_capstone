ba61852fddabfe29a1090f387a4f43bd4be47c6f
Smileyblessy <147082530+Smileyblessy@users.noreply.github.com>
2024-06-20 16:24:04 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,18 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+# Uncomment the imports before you add the code
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
+app_name = 'djangoapp'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
-    path('login/', TemplateView.as_view(template_name="index.html")),
-    path('register/', TemplateView.as_view(template_name="index.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # # path for registration
+    path(route='register', view=views.registration, name='registration'),
+    # path for login
+    path(route='login', view=views.login_user, name='login'),
+    # path for logout
+    path(route='logout', view=views.logout_request,
+         name='logout'),   
+    path(route='get_cars', view=views.get_cars, name ='getcars'),]
